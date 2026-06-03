140c49c144f70d19076550788c1fb2023cdbe8b5
Денис <777218888@gmail.com>
2026-06-03 17:07:35 +0300

    LAB6

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<int:category_id>/', views.category_view, name='category'),
     path('about/', views.about, name='about'),
+]from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<int:category_id>/', views.category_view, name='category'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,34 +2,25 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
-def home(request):
-    # Витягуємо ВСІ товари і ВСІ категорії з бази
-    products = Product.objects.all()
-    categories = Category.objects.all()
-
-    context = {
-        'page_title': 'Головна - MotoGrand',
-        'products': products,
-        'categories': categories,
-    }
-    return render(request, 'shop/home.html', context)
-
-
+# Це для сторінки категорії (щоб було видно тільки товари цієї категорії)
 def category_view(request, category_id):
-    # Витягуємо товари тільки конкретної категорії
-    categories = Category.objects.all()  # Потрібно для меню
+    categories = Category.objects.all()
     category = get_object_or_404(Category, id=category_id)
-    products = Product.objects.filter(category=category)
+    products = Product.objects.filter(category=category)  # ФІЛЬТРАЦІЯ
 
-    context = {
-        'page_title': f'Категорія: {category.name}',
+    return render(request, 'shop/home.html', {
         'products': products,
         'categories': categories,
-        'current_category': category,
-    }
-    return render(request, 'shop/home.html', context)
+        'current_category': category
+    })
 
 
-def about(request):
+# Це для сторінки конкретного товару
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
     categories = Category.objects.all()
-    return render(request, 'shop/about.html', {'page_title': 'Про нас', 'categories': categories})
+
+    return render(request, 'shop/product.html', {
+        'product': product,
+        'categories': categories
+    })
